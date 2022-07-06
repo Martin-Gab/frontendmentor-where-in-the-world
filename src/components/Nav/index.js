b2f0a88a0439e7b81fc0bfbad9578db8599737d3
Martin-Gab <martingabillet@gmail.com>
2022-07-06 14:19:33 +0200

    JSX & css OK

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -7,8 +7,21 @@
 // ====================
 import './nav.scss';
 
+// ======================
+// === Package Import ===
+// ======================
+import { Moon } from 'react-feather';
+
 const Nav = () => (
-  <div>Ici la Nav</div>
+  <div className="nav-container">
+    <div className="nav">
+      <h1 className="nav__title">Where in the world ?</h1>
+      <div className="nav__switch-mode">
+        <Moon className="nav__switch-mode__icon" size={20} strokeWidth={1.5} />
+        <p className="nav__switch-mode__text">Dark Mode</p>
+      </div>
+    </div>
+  </div>
 );
 
 export default Nav;
