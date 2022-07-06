3051fb338f42c5d77dcd8ced8076afecf8f33d7a
Martin-Gab <martingabillet@gmail.com>
2022-07-06 11:02:40 +0200

    clean project

---
+++ src/components/App/index.js
@@ -0,0 +1,18 @@
+// =====================
+// === App Component ===
+// =====================
+
+// ====================
+// === Local Import ===
+// ====================
+import './app.scss';
+
+function App() {
+  return (
+    <div className="app">
+      Ici le composant App
+    </div>
+  );
+}
+
+export default App;
